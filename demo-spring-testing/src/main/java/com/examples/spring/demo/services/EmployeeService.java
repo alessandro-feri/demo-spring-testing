8cfb2933a23fe0df48c60bd2501dcaac9c0e1aaa
alessandro <alessandro.feri@stud.unifi.it>
2020-11-16 16:15:42 +0100

    implemented "edit" end-point

--- demo-spring-testing/src/main/java/com/examples/spring/demo/services/EmployeeService.java
+++ demo-spring-testing/src/main/java/com/examples/spring/demo/services/EmployeeService.java
@@ -12,5 +12,10 @@ public class EmployeeService {
 		throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
 		
 	}
+
+	public Employee getEmployeeById(long id) {
+		throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
+		
+	}
 	
 }
