f184faa42f0de355bb493f1133ba2641287d5c53
alessandro <alessandro.feri@stud.unifi.it>
2020-11-17 16:36:42 +0100

    first failing test for edit with no employee

--- demo-spring-testing/src/test/java/com/examples/spring/demo/controllers/EmployeeWebControllerHtmlUnitTest.java
+++ demo-spring-testing/src/test/java/com/examples/spring/demo/controllers/EmployeeWebControllerHtmlUnitTest.java
@@ -67,7 +67,19 @@ public class EmployeeWebControllerHtmlUnitTest {
 						"2	test2	2000"
 				);
 	}
+	
 
+	@Test
+	public void testEditNonExistentEmployee() throws Exception {
+		when(employeeService.getEmployeeById(1L))
+			.thenReturn(null);
+
+		HtmlPage page = this.webClient.getPage("/edit/1");
+
+		assertThat(page.getBody().getTextContent())
+			.contains("No employee found with id: 1");
+	}
+	
 	//rimuove gli \r aggiuntivi prodotti da Windows per andare a capo
 	private String removeWindowsCR(String s) {
 		return s.replaceAll("\r", "");
